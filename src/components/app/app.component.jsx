e5602ffb0e3f7c5a5d3c23f56967ad0c0fb99b85
Rahul <rahulvaidya148@gmail.com>
2022-07-05 08:33:58 +0530

    commit basic design for home page

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -2,10 +2,35 @@ import "./app.component.css";
 
 const App = () => {
   return (
-    <div className="App-header">
-      
-        <h1>Happy Hacking :)</h1>
-     
+    <div className="main-page-container">
+      {/* navbar  */}
+      <div className="navbar flex">
+        {/* left-navbar */}
+        <div className="navbar-left">
+          <div className="navbar-search-container">
+            <label htmlFor="search">Search for stuff</label>
+            <input
+              id="search"
+              type="number"
+              min={1000}
+              max={10000}
+              placeholder="Search...."
+              autoFocus
+            />
+          </div>
+        </div>
+
+        {/* right navbar */}
+        <div className="navbar-right">
+        <button>sort by Price (ASC)</button><button>sort by Price (DESC)</button>
+        </div>
+      </div>
+
+    {/* main section */}
+    <div className="products-container flex">
+
+    </div>
+
     </div>
   );
 };
