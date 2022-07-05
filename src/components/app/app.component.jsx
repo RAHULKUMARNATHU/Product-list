218f8ce2223f94a69482736a1d7f6bec673d9efc
Rahul <rahulvaidya148@gmail.com>
2022-07-05 08:03:21 +0530

    created a directory structure

---
+++ src/components/app/app.component.jsx
@@ -0,0 +1,13 @@
+import "./app.component.css";
+
+const App = () => {
+  return (
+    <div className="App-header">
+      
+        <h1>Happy Hacking :)</h1>
+     
+    </div>
+  );
+};
+
+export default App;
